61242a2f460636b56a5cd6b74894128c8d438c72
synthresin <synthresin@me.com>
2013-08-01 17:27:44 +0900

    이미지 업로드 제외한 부분 완료

--- app/controllers/admin/works_controller.rb
+++ app/controllers/admin/works_controller.rb
@@ -1,3 +1,44 @@
 class Admin::WorksController < AdminController
-	before_action :authenticate_admin!
+
+	def index
+		@works = Work.search(params[:search]).paginate(:page => params[:page], :per_page => 10)
+	end
+
+	def new
+		@work = Work.new
+	end
+
+	def create
+		@work = Work.new(work_params)
+
+		if @work.save
+			redirect_to admin_works_path, notice: '작업을 성공적으로 생성하였습니다.'
+		else
+			render action: 'new'
+		end
+	end
+
+	def edit
+		@work = Work.find(params[:id])
+	end
+
+	def update
+		@work = Work.find(params[:id])
+		if @work.update(work_params)
+			redirect_to admin_works_path, notice: '작업을 성공적으로 수정하였습니다.'
+		else
+			render action: 'edit'
+		end
+	end
+
+	def destroy
+		Work.find(params[:id]).destroy
+		redirect_to admin_works_path, notice: '작업을 성공적으로 삭제하였습니다.'
+	end
+
+	private
+
+	def work_params
+		params.require(:work).permit(:title, :client, :body, :category_id)
+	end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,11 @@
 class Work < ActiveRecord::Base
 	belongs_to :category
+
+	def self.search(search)
+	  if search && !search.blank?
+	    where("title LIKE ?", '%' + search + '%' )
+	  else
+	    all
+	  end
+	end
 end
